ad212e619f4e338a5c10890781d17a59dc844e51
admin <subhashispanda996@gmail.com>
2021-01-15 14:17:28 +0530

    First Commit
    Uploading all the files

---
+++ MyCode/src/com/deb/java/Calculator.java
@@ -0,0 +1,11 @@
+package com.deb.java;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println(Operators.add(5, 2));
+		System.out.println(Operators.sub(5, 2));
+
+	}
+
+}
---
+++ MyCode/src/com/deb/java/Operators.java
@@ -0,0 +1,12 @@
+package com.deb.java;
+
+public class Operators {
+	
+	public static int add(int n1,int n2) {
+		return n1+n2;
+	}
+	public static int sub(int n1,int n2) {
+		return n1-n2;
+	}
+
+}
